af1acb480bd8fd48751ab455b63e33fc56bb61d2
Viacheslav <564439@mail.muni.cz>
2026-04-24 16:24:04 +0200

    place holder views and settings created

--- Project/reminder/reminder/MauiProgram.cs
+++ Project/reminder/reminder/MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using reminder.Services;
+using reminder.ViewModels;
 
 namespace reminder
 {
@@ -14,9 +16,19 @@ namespace reminder
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+            builder.Services.AddSingleton(provider =>
+            {
+                var ip = Preferences.Get("PicoIp", "");
+                return new PicoClient(ip);
+            });
+
+            builder.Services.AddSingleton<EventStorageService>();
+
+            builder.Services.AddSingleton<EventsViewModel>();
+            builder.Services.AddSingleton<MainPage>();
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
---
+++ Project/reminder/reminder/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+public partial class SettingsViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string picoIp = Preferences.Get("PicoIp", "");
+
+    [RelayCommand]
+    private void Save()
+    {
+        Preferences.Set("PicoIp", PicoIp);
+    }
+}
