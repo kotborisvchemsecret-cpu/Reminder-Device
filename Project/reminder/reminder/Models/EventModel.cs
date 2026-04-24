8c5309b2931acfb5b7eb9f37ed42d3277a73d232
Viacheslav <564439@mail.muni.cz>
2026-04-24 15:13:05 +0200

    created project added event template and functions to load and save whole event lists

---
+++ Project/reminder/reminder/Models/EventModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace reminder.Models
+{
+    internal class Class1
+    {
+    }
+}
---
+++ Project/reminder/reminder/Services/EventStorageService.cs
@@ -0,0 +1,23 @@
+﻿using reminder.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace reminder.Services
+{
+    public class EventStorageService
+    {
+        public async Task SaveAsync(string path, IEnumerable<EventModel> events)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            await File.WriteAllTextAsync(path, JsonSerializer.Serialize(events, options));
+        }
+
+        public async Task<List<EventModel>> LoadAsync(string path)
+        {
+            var json = await File.ReadAllTextAsync(path);
+            return JsonSerializer.Deserialize<List<EventModel>>(json) ?? new();
+        }
+    }
+}
